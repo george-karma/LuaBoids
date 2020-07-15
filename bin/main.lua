b66ba29040b207fa0cf87bd6c6cfdb91962612ed
George Nitu <danut.nitu@outlook.com>
2020-07-15 20:06:38 +0100

    Added Profiler, removed old boid

--- bin/main.lua
+++ bin/main.lua
@@ -6,14 +6,21 @@ Input= require "input"
 Physics = require 'windfield'
 globals = require "globals"
 Vector = require 'vector'
+profiler = require("profiler")
 
 
 
 function love.load()
+profiler.start()
 readObjects("Objects")
 timer = Timer()
 input = Input()
 screenController = ScreenControler()
+timer:after(30,function()
+    profiler.stop()
+    profiler.report("profiler.log")
+    love.event.quit()
+    end)
 
 end
 
